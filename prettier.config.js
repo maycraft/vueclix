61a026fcf7dec3d49b9f078e8facbf5ac8a202eb
Sergey Ryazantsev <rjazantsev@gmail.com>
2021-12-20 20:55:50 +0300

    Added config file for prettier

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    tabWidth: 4,
+    singleQuote: true,
+    trailingComma: 'all',
+    semi: true,
+    jsxBracketSameLine: true,
+    arrowParens: 'avoid',
+    bracketSpacing: true,
+    endOfLine: 'auto',
+    printWidth: 100,
+};
