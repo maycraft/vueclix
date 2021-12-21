bd02de93de5f6f239ab648b3f727e6f0d0d77aaf
Sergey Ryazantsev <rjazantsev@gmail.com>
2021-12-21 17:07:02 +0300

    Refactored adding basic styles and fonts

--- vue.config.js
+++ vue.config.js
@@ -2,13 +2,13 @@ module.exports = {
     css: {
         loaderOptions: {
             sass: {
-                prependData: `
-                    @import "@/assets/scss/_variables.scss";
-                    @import "@/assets/scss/_fonts.scss";
-                    @import "@/assets/scss/lib/_custom-grid.scss";
-                    @import "@/assets/scss/lib/_normalize.scss";
-                `,
+                prependData: `@import "@/assets/scss/main.scss";`,
             },
         },
     },
 };
+
+// @import "@/assets/scss/_variables.scss";
+// @import "@/assets/scss/_fonts.scss";
+// @import "@/assets/scss/lib/_custom-grid.scss";
+// @import "@/assets/scss/lib/_normalize.scss";
