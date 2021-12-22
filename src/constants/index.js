4a966bada460518193c745fb2bfbe8db7b69fc22
Sergey Ryazantsev <rjazantsev@gmail.com>
2021-12-22 16:52:41 +0300

    Added file with contants for api

---
+++ src/constants/index.js
@@ -0,0 +1,44 @@
+export const API_KEY = '?api_key=' + process.env.VUE_APP_MOVIEDB_API_KEY;
+
+const API_URL = 'https://api.themoviedb.org/3';
+
+const API_CATEGORY_MOVIE = '/movie';
+
+const API_CATEGORY_GENRE = '/genre/movie/list';
+
+const API_TYPE_UPCOMING = '/upcoming';
+
+const API_TYPE_SEARCH = '/search';
+
+const API_TYPE_NOW_PLAYING = '/now_playing';
+
+// const API_TYPE_POPULAR = '/popular';
+
+// const API_TYPE_TOP_RATED = '/top_rated';
+
+// const API_REGION_RU = '&language=ru&region=ru'
+const API_REGION_RU = '&language=ru-RU&region=RU';
+
+const API_PAGE_PARAM = '&page=';
+
+const API_QUERY_PARAM = '&query=';
+
+export const API_CONSTANT = API_KEY + API_REGION_RU;
+
+export const API_URL_MOVIE = API_URL + API_CATEGORY_MOVIE;
+
+export const API_URL_UPCOMING = API_URL_MOVIE + API_TYPE_UPCOMING + API_CONSTANT + API_PAGE_PARAM;
+
+export const API_URL_NOW_PLAYING =
+    API_URL_MOVIE + API_TYPE_NOW_PLAYING + API_CONSTANT + API_PAGE_PARAM;
+
+export const API_URL_GENRE = API_URL + API_CATEGORY_GENRE + API_CONSTANT;
+
+export const API_URL_SEARCH =
+    API_URL + API_TYPE_SEARCH + API_CATEGORY_MOVIE + API_CONSTANT + API_QUERY_PARAM;
+
+export const POSTER_URL_SM = 'https://image.tmdb.org/t/p/w300';
+
+export const POSTER_URL_MD = 'https://image.tmdb.org/t/p/w500';
+
+export const YOUTUBE_URL = 'https://www.youtube.com/embed/';
