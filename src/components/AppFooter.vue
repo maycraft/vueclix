6b13eec9c9fb996066a1467d68a1cbfe58ac5540
Sergey Ryazantsev <rjazantsev@gmail.com>
2021-12-22 16:50:56 +0300

    Created AppFooter component

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="footer">
+        maycraft &copy;
+        <span class="date"> {{ new Date().getFullYear() }}</span>
+    </div>
+</template>
+<script>
+export default {
+    name: 'AppFooter',
+};
+</script>
+<style lang="scss">
+.footer {
+    padding: 20px 0;
+    color: $white;
+    text-align: center;
+
+    .date {
+        color: $white;
+    }
+}
+</style>
