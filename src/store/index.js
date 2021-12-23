cfd9912e825efb192de1362fdda4973ec03cfdff
Sergey Ryazantsev <rjazantsev@gmail.com>
2021-12-23 16:48:54 +0300

    Added genres state, and getting him from rest api in store

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,22 @@
 import { createStore } from 'vuex';
-
+import { getGenres } from '@/api';
 export default createStore({
-    state: {},
-    mutations: {},
-    actions: {},
+    state: {
+        genres: [],
+    },
+    getters: {
+        genres: state => state.genres,
+    },
+    mutations: {
+        setGenres(state, genres) {
+            state.genres = genres;
+        },
+    },
+    actions: {
+        async getGenres({ commit }) {
+            const genres = await getGenres();
+            commit('setGenres', genres);
+        },
+    },
     modules: {},
 });
