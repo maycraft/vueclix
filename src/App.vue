b4fc3250bb11c593fb51f5ad2a2314928433328c
Sergey Ryazantsev <rjazantsev@gmail.com>
2021-12-23 17:00:27 +0300

    Added AppFooter in the app

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,17 @@
     <div className="container">
         <app-header />
         <router-view></router-view>
+        <app-footer></app-footer>
     </div>
 </template>
 <script>
 import AppHeader from '@/components/AppHeader';
+import AppFooter from '@/components/AppFooter';
 
 export default {
     components: {
         AppHeader,
+        AppFooter,
     },
     created() {
         this.$store.dispatch('getGenres');
