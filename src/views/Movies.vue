2daf8d8df8c4e02e636eddfb0c22f6f9e9385965
Sergey Ryazantsev <rjazantsev@gmail.com>
2021-12-23 16:43:47 +0300

    Add rendering movie cards in Movies component

---
+++ src/views/Movies.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="cards">
+        <movie-card
+            v-for="movie in movies"
+            :key="movie.id"
+            :id="movie.id"
+            :poster="movie.poster_path"
+            :title="movie.title"
+            :release="movie.release_date"
+            :genres="movie.genre_ids"
+        ></movie-card>
+    </div>
+</template>
+
+<script>
+import MovieCard from '@/components/MovieCard.vue';
+import { mapGetters } from 'vuex';
+
+export default {
+    name: 'Movies',
+    components: {
+        MovieCard,
+    },
+    created() {
+        this.$store.dispatch('getNewMovies', 1);
+    },
+    computed: {
+        ...mapGetters(['movies', 'currentPage', 'totalPages']),
+    },
+};
+</script>
+<style lang="scss">
+.cards {
+    @extend .row;
+    --bs-gutter-x: 30px;
+    border-radius: 5px;
+    background-color: $white;
+    padding-top: calc(var(--bs-gutter-x) / 2);
+    padding-bottom: calc(var(--bs-gutter-x) / 2);
+    justify-content: flex-start;
+}
+.card {
+    max-width: 350px;
+    margin-bottom: 0.5rem;
+    display: flex;
+    flex-direction: column;
+    cursor: pointer;
+
+    &__image {
+        position: relative;
+        margin-bottom: 0.5rem;
+        overflow: hidden;
+        padding-bottom: 150%;
+        img {
+            position: absolute;
+            top: 0;
+            right: 0;
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+    }
+
+    &__title {
+        margin-bottom: 0.5rem;
+    }
+
+    &__genres {
+        font-size: 0.75rem;
+        color: #0277bd;
+        min-height: 1.5rem;
+        margin-top: auto;
+    }
+}
+
+@include media-breakpoint-up(sm) {
+    .card {
+        @include make-col(6);
+    }
+}
+@include media-breakpoint-up(md) {
+    .card {
+        @include make-col(4);
+    }
+}
+@include media-breakpoint-up(lg) {
+    .card {
+        @include make-col(3);
+    }
+}
+</style>
