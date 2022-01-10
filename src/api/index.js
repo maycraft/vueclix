69105cf6f065b13bdfca5ca71d09b37f8018db17
Sergey Ryazantsev <rjazantsev@gmail.com>
2022-01-10 10:42:19 +0300

    Added function for fetch search query in api

--- src/api/index.js
+++ src/api/index.js
@@ -4,7 +4,7 @@ import {
     API_CONSTANT,
     API_URL_GENRE,
     API_URL_NOW_PLAYING,
-    // API_URL_SEARCH,
+    API_URL_SEARCH,
 } from '../constants';
 
 function handleError(err) {
@@ -34,9 +34,8 @@ export const getGenres = async () => {
 export const getMovieById = async id => {
     return await getData(`${API_URL_MOVIE}/${id}${API_CONSTANT}&append_to_response=videos,credits`);
 };
-// getSearchMovies(query) {
-//     const value = query ? query : '';
-//     return this.getData(API_URL_SEARCH + value);
-// },
+export const getSearchMovies = async query => {
+    return await getData(API_URL_SEARCH + query);
+};
 
-// getUpcomingMovies(page) {},
+// getUpcomingMovies(page) {}
