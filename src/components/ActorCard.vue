24001ec1ef849c6e2a2fe396f18ff35f6e526d82
Sergey Ryazantsev <rjazantsev@gmail.com>
2022-01-17 18:00:58 +0300

    Render no-image file depend on prop gender

--- src/components/ActorCard.vue
+++ src/components/ActorCard.vue
@@ -3,29 +3,12 @@
         <figure class="actor__avatar">
             <img :src="imageURL" :alt="name" v-if="imagePath" />
             <div class="actor__avatar_slug" v-else>
-                <svg
-                    version="1.1"
-                    id="Capa_1"
-                    xmlns="http://www.w3.org/2000/svg"
-                    x="0px"
-                    y="0px"
-                    viewBox="0 0 513.323 513.323"
-                >
-                    <g>
-                        <g>
-                            <path
-                                d="M256.661,257.323c-135.275,0-245.333,110.059-245.333,245.333c0,5.888,4.779,10.667,10.667,10.667h469.333
-                                        c5.888,0,10.667-4.779,10.667-10.667C501.995,367.381,391.936,257.323,256.661,257.323z"
-                                fill="#ffffff"
-                            />
-                        </g>
-                    </g>
-                    <g>
-                        <g>
-                            <circle cx="256.661" cy="130.333" r="117.333" fill="#ffffff" />
-                        </g>
-                    </g>
-                </svg>
+                <img
+                    v-if="gender === 1"
+                    src="@/assets/img/no_female_image.svg"
+                    alt="female avatar"
+                />
+                <img v-else src="@/assets/img/no_male_image.svg" alt="male avatar" />
             </div>
         </figure>
         <p class="actor__name">{{ name }}</p>
@@ -53,6 +36,10 @@ export default {
             type: String,
             required: true,
         },
+        gender: {
+            type: Number,
+            required: true,
+        },
     },
     computed: {
         imageURL() {
@@ -70,12 +57,11 @@ export default {
 
     &__avatar {
         &_slug {
-            padding-top: 48%;
-            background: lightgray;
+            background: #e6e6e6;
             max-width: 300px;
-            margin: 0 auto;
         }
 
+        svg,
         img {
             max-width: 100%;
         }
@@ -89,23 +75,6 @@ export default {
     &__character {
         font-size: 0.75rem;
         color: #9e9e9e;
-    }
-}
-
-@include media-breakpoint-up(sm) {
-    .actor {
-        @include make-col(6);
-    }
-}
-@include media-breakpoint-up(md) {
-    .actor {
-        @include make-col(3);
-    }
-}
-
-@include media-breakpoint-up(lg) {
-    .actor {
-        @include make-col(2);
     }
 }
 </style>
