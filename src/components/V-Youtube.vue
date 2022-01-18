b9d7e2760866df1869b765e073aac0b5c0a2f453
Sergey Ryazantsev <rjazantsev@gmail.com>
2022-01-18 11:09:03 +0300

    Created v-youtube component

---
+++ src/components/V-Youtube.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="relation video">
+        <div class="relation__ratio">
+            <v-loader
+                v-if="!isLoaded"
+                loader="bars"
+                :isFullPage="false"
+                color="#FF0101"
+                :opacity="1"
+            ></v-loader>
+            <iframe
+                v-show="isLoaded"
+                @load="loadVideo"
+                class="relation__content"
+                :src="'https://www.youtube.com/embed/' + videoKey"
+                title="YouTube video player"
+                allowFullScreen
+            ></iframe>
+        </div>
+    </div>
+</template>
+<script>
+import VLoader from '@/components/V-Loader';
+export default {
+    name: 'v-youtube',
+    props: {
+        videoKey: {
+            type: String,
+            required: true,
+        },
+    },
+    components: {
+        VLoader,
+    },
+    data() {
+        return {
+            isLoaded: false,
+        };
+    },
+    methods: {
+        loadVideo() {
+            this.isLoaded = true;
+        },
+    },
+};
+</script>
+<style lang="scss">
+.relation {
+    margin: 0 auto;
+    position: relative;
+}
+
+.relation__ratio {
+    padding-top: 56.25%;
+    height: 0;
+}
+
+.relation__content {
+    position: absolute;
+    border: none;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+}
+
+.video {
+    max-width: 700px;
+    margin-bottom: 1rem;
+}
+</style>
