1b397317ee1be0f78711e628d1f5d6c18528673f
Sergey Ryazantsev <rjazantsev@gmail.com>
2022-01-25 15:03:37 +0300

    Changed arrow in scroll to top button

--- src/components/ScrollTop.vue
+++ src/components/ScrollTop.vue
@@ -2,24 +2,25 @@
     <transition name="scroll">
         <button class="scroll-top" @click="$emit('scroll')" v-if="show">
             <svg
-                version="1.0"
+                version="1.1"
+                id="Layer_1"
                 xmlns="http://www.w3.org/2000/svg"
-                width="100%"
-                height="100%"
-                viewBox="0 0 50.000000 50.000000"
-                preserveAspectRatio="xMidYMid meet"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                x="0px"
+                y="0px"
+                width="25px"
+                height="25px"
+                viewBox="0 0 330 330"
+                style="enable-background: new 0 0 330 330"
+                xml:space="preserve"
             >
-                <g
-                    transform="translate(0.000000,50.000000) scale(0.100000,-0.100000)"
-                    fill="#ffffff"
-                    stroke="none"
-                >
-                    <path
-                        d="M185 360 c-32 -33 -56 -63 -52 -67 4 -4 30 15 57 42 l50 49 0 -152
-        c0 -95 4 -152 10 -152 6 0 10 57 10 152 l0 152 50 -49 c27 -27 53 -46 57 -42
-        6 7 -104 127 -117 127 -3 0 -32 -27 -65 -60z"
-                    />
-                </g>
+                <path
+                    id="XMLID_29_"
+                    d="M100.606,100.606L150,51.212V315c0,8.284,6.716,15,15,15c8.284,0,15-6.716,15-15V51.212l49.394,49.394
+	C232.322,103.535,236.161,105,240,105c3.839,0,7.678-1.465,10.606-4.394c5.858-5.857,5.858-15.355,0-21.213l-75-75
+	c-5.857-5.858-15.355-5.858-21.213,0l-75,75c-5.858,5.857-5.858,15.355,0,21.213C85.251,106.463,94.749,106.463,100.606,100.606z"
+                    fill="#fff"
+                />
             </svg>
         </button>
     </transition>
@@ -63,6 +64,11 @@ export default {
     bottom: 15%;
     padding: 0;
     background: $blue;
+
+    // svg {
+    //     width: 25px;
+    //     height: 25px;
+    // }
 }
 
 .scroll-enter-active,
