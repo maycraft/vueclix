939fb99f4856a8e9ec4ace2b3c6f43f3d5767736
Sergey Ryazantsev <rjazantsev@gmail.com>
2022-02-03 17:25:03 +0300

    Refactoring main style files

--- src/App.vue
+++ src/App.vue
@@ -20,50 +20,16 @@ export default {
 };
 </script>
 <style lang="scss">
-html,
-body {
-    font-size: 16px;
+#app {
     position: relative;
-    font-family: 'Helvetica Neue Cyr', sans-serif;
-
-    @media screen and (max-width: 576px) {
-        font-size: 14px;
-    }
-}
-
-body {
-    background: url('./assets/img/background.jpg') top center no-repeat;
-    background-size: cover;
-    background-attachment: fixed;
-}
-
-p,
-span {
-    @media screen and (max-width: 576px) {
-        font-size: 1.1em;
-    }
-}
-
-#root {
     width: 100%;
-    position: relative;
 }
 
-.bold {
-    font-weight: bold;
-}
+// .bold {
+//     font-weight: bold;
+// }
 
-.gy-3 {
-    margin: 1rem 0;
-}
-
-.container {
-    @include make-container();
-    @include make-max-width-container();
-
-    @media screen and (max-width: 576px) {
-        padding-right: var(--bs-gutter-x, 1.5rem);
-        padding-left: var(--bs-gutter-x, 1.5rem);
-    }
-}
+// .gy-3 {
+//     margin: 1rem 0;
+// }
 </style>
