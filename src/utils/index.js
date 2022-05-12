83773bde0adf3bb50ba4fc53cebb6f8a0ac0619c
sr <C:\Users\sr\AppData\Roaming\The Bat!>
2022-05-12 21:47:19 +0300

    Changing helper function to new api

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,27 +1,35 @@
-import store from '@/store';
+const months = ['JANUARY', 'FEBRUARY', 'MARCH', 'APRIL', 'MAY', 'JUNE', 'JULY', 'SEPTEMBER', 'OCTOBER', 'NOVEMBER', 'DECEMBER'];
 
-export const getReleaseDate = str => (str ? str.split('-')[0] : '');
+export const getCurrentMonth = () => {
+    const date = new Date();
+    return months[date.getMonth()];
+}
 
-export const getMovieGenres = genreIds => {
+export const getMovieGenres = genresArr => {
     const res = [];
-    store.getters.genres.forEach(genre => {
-        for (let i = 0; i < genreIds.length; i++) {
-            if (genre.id === genreIds[i]) res.push(genre.name);
-        }
-    });
+    genresArr.forEach(item => {
+        res.push(item.genre);
+    })
     return res.join(', ');
 };
+
+export const mapCrewItem = (items, department) => {
+    return items
+        .filter(item => item.professionKey === department)
+        .map(item => item.nameRu || item.nameEn)
+        .join(', ');
+};
+
+export const getMovieCountries = countriesArr => {
+    return countriesArr.map(item => item.country).join(', ');
+}
+
+export const getReleaseDate = str => (str ? str.split('-')[0] : '');
 
 export const showGenres = genres => genres.map(genre => genre.name).join(', ');
 
 export const mapItems = items => items.map(item => item.name).join(', ');
 
-export const mapCrewItem = (items, department) => {
-    return items
-        .filter(item => item.department.includes(department))
-        .map(item => item.name)
-        .join(', ');
-};
 
 export const isNumeric = value =>
     !isNaN(value) && parseInt(value[0]) !== 0 ? parseInt(value) : null;
