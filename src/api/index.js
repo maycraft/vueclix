91d2a080f6f50e90e30c660fb925fec3e5120ed8
sr <C:\Users\sr\AppData\Roaming\The Bat!>
2022-05-15 20:17:00 +0300

    Changed async functions for the new api

--- src/api/index.js
+++ src/api/index.js
@@ -1,78 +1,89 @@
 import axios from 'axios';
 import store from '@/store';
 import {
-    API_URL_MOVIE,
-    API_CONSTANT,
-    API_URL_GENRE,
-    API_URL_NOW_PLAYING,
-    API_URL_SEARCH,
-    API_URL_UPCOMING,
-    API_URL_AUTH,
-    API_URL_RATING,
-    API_KEY,
-    API_URL_PERSON,
+    API_PARAM_PAGE,
+    API_URL_TOP_100_POPULAR_FILMS,
+    API_URL_CATEGORY_FILMS,
+    API_URL_STAFF,
+    API_URL_SEARCH_FILMS,
+    API_URL_PERSON
 } from '../constants';
 
-function handleError(err) {
-    throw err;
-}
+const KinoPoiskClient = axios.create({
+    baseURL: 'https://kinopoiskapiunofficial.tech/api',
+    timeout: 1500,
+    headers: {
+        'X-API-KEY': process.env.VUE_APP_KINOPOISK_API_KEY,
+        'Content-Type': 'application/json',
+    },
+})
+
+const YouTubeClient = axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3/'
+})
 
 async function getData(url) {
     store.dispatch('setLoading', true);
     store.dispatch('setError', null);
     try {
-        const res = await axios.get(url);
+        const res = await KinoPoiskClient.get(url);
         if (res.status === 200) {
             return res.data;
         } else {
+            console.log('errer');
             throw new Error(`Не удалось получить данные с ${url}`);
         }
     } catch (err) {
-        handleError(err);
+        console.log('error2', err.response);
+        if (err.response) {
+            return err.response.data;
+        } else {
+            store.dispatch('setError', err.message);
+        }
     } finally {
         store.dispatch('setLoading', false);
     }
 }
 
-export const getGenres = async () => {
-    const res = await getData(API_URL_GENRE);
-    return res.genres;
-};
 export const getNewMovies = async page => {
-    return await getData(API_URL_NOW_PLAYING + page);
-};
-export const getUpcomingMovies = async page => {
-    return await getData(API_URL_UPCOMING + page);
-};
-export const getMovieById = async id => {
-    return await getData(`${API_URL_MOVIE}/${id}${API_CONSTANT}&append_to_response=videos,credits`);
-};
-export const getSearchMovies = async query => {
-    return await getData(API_URL_SEARCH + query);
+    return await getData(API_URL_TOP_100_POPULAR_FILMS + page);
+    // return await getData(API_URL_FILMS_PRIMIERES + '2022' + API_PARAM_MONTH + getCurrentMonth());
 };
 
-export const generateSessionID = async () => {
-    const res = await axios.get(API_URL_AUTH);
-    return res.data.guest_session_id;
+const getStaffByMovieId = async id => {
+    return await getData(API_URL_STAFF + id)
+}
+
+const getTrailerByMovieId = async id => {
+    return await getData(`${API_URL_CATEGORY_FILMS}/${id}/videos`);
+}
+
+export const getMovieById = async id => {
+    const data = [];
+    const mainData = await getData(`${API_URL_CATEGORY_FILMS}/${id}`);
+    const staff = await getStaffByMovieId(id);
+    const trailers = await getTrailerByMovieId(id);
+    data.push(mainData, staff, trailers)
+    return data;
 };
-export const postRating = async (movieID, rating, sessionID) => {
-    const url = `${API_URL_RATING}/${movieID}/rating${API_KEY}&guest_session_id=${sessionID}`;
+
+export const getVideosByName = async movieTitle => {
     try {
-        const res = await axios.post(url, JSON.stringify({ value: parseInt(rating) }), {
-            headers: {
-                'Content-Type': 'application/json;charset=UTF-8',
-            },
-        });
-        return res.data.success;
+        const videosAll = await YouTubeClient.get(`search?part=snippet&q=Трейлер ${movieTitle}&type=video&key=${process.env.VUE_APP_YOUTUBE_API_KEY}`)
+        return videosAll.data.items.map(item => ({
+            videoId: item.id.videoId,
+            title: item.snippet.title,
+            thumbnailUrl: item.snippet.thumbnails.high.url
+        }))
     } catch (err) {
-        if (err.response) {
-            console.error(err.response.data);
-        } else {
-            console.error(err.message);
-        }
+        return [];
     }
+}
+
+export const getSearchMovies = async query => {
+    return await getData(API_URL_SEARCH_FILMS + query + API_PARAM_PAGE + '1');
 };
 
 export const getActorById = async id => {
-    return await getData(`${API_URL_PERSON}/${id}${API_CONSTANT}&append_to_response=movie_credits`);
+    return await getData(API_URL_PERSON + id);
 };
