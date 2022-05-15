acad646d17d751ececbaf0dd5dcf8b7982ac9cc2
sr <C:\Users\sr\AppData\Roaming\The Bat!>
2022-05-15 22:08:49 +0300

    Testing deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/vueclix/' : '/',
+    // publicPath: process.env.NODE_ENV === 'production' ? '/vueclix/' : '/',
     css: {
         loaderOptions: {
             sass: {
